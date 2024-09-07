0cb6ffda286049cb6ea152c1283a659f00cc4127
stonseee <stonseee@STONSEEE>
2024-09-07 21:57:19 +0300

    STD vector in progress

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,8 +1,67 @@
-#include <iostream>
-using namespace std;
+﻿#include<iostream>
+#include<array>
+#include<vector>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n-----------------------------------\n"
+
+//#define STL_ARRAY
+#define STL_VECTOR
+
+template<typename T>void vector_info(const std::vector<T>& vec);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hi STL!!" << endl;
+	
+#ifdef STL_ARRAY
+	
+	const int N = 5;
+	std::array<int, N> arr = { 3, 5, 8, 13, 21 };
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+#endif // STL_ARRAY
+
+#ifdef STL_VECTOR
+
+	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	cout << endl;
+	vector_info(vec);
+	vec.push_back(55);
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec.at(i) << tab;
+	}
+	cout << endl;
+	vector_info(vec);
+	vec.reserve(24);
+	vec.shrink_to_fit();
+	vec.resize(8);
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	vector_info(vec);
+
+#endif // STL_VECTOR
+
+}
+
+template<typename T>void vector_info(const std::vector<T>& vec)
+{
+	cout << "Size:     " << vec.size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl;
+	cout << "MaxSize:  " << vec.max_size() << endl;
 }
