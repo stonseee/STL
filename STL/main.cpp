ec0b320493bdf1af1bacc1d1f59252b26c86394e
stonseee <stonseee@STONSEEE>
2024-09-11 18:51:13 +0300

    Homework

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include<iostream>
 #include<array>
 #include<vector>
+#include<list>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -9,7 +10,9 @@ using std::endl;
 #define delimiter "\n-----------------------------------\n"
 
 //#define STL_ARRAY
-#define STL_VECTOR
+//#define STL_VECTOR
+//#define VECTOR_FLEXABILITY
+//#define LIST_FLEXABILITY
 
 template<typename T>void vector_info(const std::vector<T>& vec);
 
@@ -31,11 +34,11 @@ void main()
 
 #ifdef STL_VECTOR
 
-	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };		
 	for (int i = 0; i < vec.size(); i++)
 	{
 		cout << vec[i] << tab;
-	}
+	}	
 	cout << endl;
 	vector_info(vec);
 	vec.push_back(55);
@@ -55,7 +58,63 @@ void main()
 	cout << endl;
 	vector_info(vec);
 
-#endif // STL_VECTOR
+#endif // STL_VECTOR	
+
+#ifdef VECTOR_FLEXABILITY
+	std::vector<int> vec = { 3, 5, 8, 13, 21 };
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	cout << endl;
+
+	std::vector<int>::iterator it;
+	it = vec.begin();
+	++it;
+	//vec.insert(it, 99);            //работает
+	vec.insert(vec.begin() + 2, 99); //работает
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	cout << endl;
+
+	vec.erase(vec.begin() + 2);      //работает
+	//vec.erase(it + 2);             //не работает
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	cout << endl;
+#endif // VECTOR_FLEXABILITY
+
+#ifdef LIST_FLEXABILITY
+	std::list<int> list = { 3, 5, 8, 13, 21 };	
+	std::list<int>::iterator it;
+
+	it = list.begin();
+	++it;
+
+	//list.insert(list.begin() + 2, 99); //не работает
+	//list.insert(list.begin(), 99);     //работает
+	list.insert(it, 99);                 //работает
+
+	++it;
+	list.erase(it);                      //работает
+	//list.erase(list.begin());          //работает
+	//list.erase(list.begin() + 3);      //не работает
+
+	int n = list.size();
+	for (int i = 0; i < n; i++)
+	{
+		cout << list.front() << tab;
+		list.pop_front();
+	}
+	cout << endl;
+
+
+#endif // LIST_FLEXABILITY
+
 
 }
 
